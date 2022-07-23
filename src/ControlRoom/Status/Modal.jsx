250a0ef1ab0415d167b0c4a14509f1e6ffaaceb4
Ariel <102249863+ArielRivas@users.noreply.github.com>
2022-07-23 22:40:54 +0200

    adding modals

---
+++ src/ControlRoom/Status/Modal.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const Modal = () => {
+  return (
+ <section>
+    <h1>React Modal</h1>
+        <button type="button" onClick={this.showModal}>
+          Open
+        </button>
+ </section>
+  )
+}
+
+export default Modal
+
--- src/ControlRoom/Status/Status.jsx
+++ src/ControlRoom/Status/Status.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import imgStatus from '../../assets/controlRoom/spaceship-g9d3838581_1920.png'
+import imgStatus from '../../assets/controlRoom/spaceship-g9d3838581_1920.png';
+import './Status.scss'
+import Dashboard from './Dashboard';
+
 
 const Status = () => {
   return (
-    <section>
-       <div><img src={imgStatus} alt='spaceship Render'></img></div>
+    <section className='container__Status'>
+       <div><img  className="container__Status-img " src={imgStatus} alt='spaceship Render'></img></div>
+       <div className="container__Status-Dashboard-a"><Dashboard/></div>
+       <div className="container__Status-Dashboard-b"><Dashboard/></div>
+       <div className="container__Status-Dashboard-c"><Dashboard/></div>
     </section>
-   
   )
 }
 
-export default Status
+export default Status;
