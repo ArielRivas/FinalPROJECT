ef4f6d4d2d14219e42304d8ef521e9b8ca7cb838
Ariel <102249863+ArielRivas@users.noreply.github.com>
2022-07-24 21:24:21 +0200

    addin redux/home/login

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from "./redux/store";
+import Home from './Home/Home';
+import Login from './Login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+    <Provider store={store}></Provider>
+    <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
      <Router><App /></Router>       
   </React.StrictMode>
 );
