b1f5a82a698f5220dde04e4935f71adb93deb1a9
Ariel <102249863+ArielRivas@users.noreply.github.com>
2022-07-24 13:18:40 +0200

    addin styles

--- src/ControlRoom/Launch/Launch.jsx
+++ src/ControlRoom/Launch/Launch.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./Launch.scss";
+import { Link } from "react-router-dom";
 
-const  Launch = () => {
+const Launch = () => {
   return (
-     <section className='container__Launch'>
+    <section className='container__Launch'>
       <div className='container__Launch-card'>
         <h3>Push the button</h3>
         <p>Reaching destination: 15 min</p>
+        <span className='container__Launch-slider'></span>
+        <Link to='/rocketvideos'>
+        <button className='container__Launch-button'>Go</button></Link>
       </div>
-      <span className='container__Launch-slider'></span>
-      <button className='container__Launch-button'>Go</button>
     </section>
-  )
-}
+  );
+};
 
-export default Launch
+export default Launch;
